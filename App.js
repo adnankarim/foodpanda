eddb0dd0df143717206dee5f6f6ed17304f66506
adnankarim <skiwordylectures@gmail.com>
2021-04-18 13:22:27 +0500

    Initial Search and List Ui

--- App.js
+++ App.js
@@ -5,3 +5,29 @@
  * @format
  * @flow strict-local
  */
+import React from 'react';
+import { View, Text, SafeAreaView, Platform, StatusBar } from 'react-native';
+
+const isAndroid = Platform.OS === 'android';
+
+const App = () => {
+  return (
+    <>
+      <SafeAreaView
+        style={{
+          flex: 1,
+          marginTop: isAndroid ? 0 : 0,
+        }}>
+        <View style={{ padding: 16, backgroundColor: 'green' }}>
+          <Text>Search</Text>
+        </View>
+        <View style={{ flex: 1, padding: 16, backgroundColor: 'blue' }}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <StatusBar StatusBarStyle='default' />
+    </>
+  );
+};
+
+export default App;
