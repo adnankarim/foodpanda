4e611cacfdd740ae5b7c485c4cb53615b08699c1
adnankarim <skiwordylectures@gmail.com>
2021-04-28 18:09:33 +0500

    Activity indicator Loader added

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -4,6 +4,7 @@ import { Searchbar } from 'react-native-paper';
 import { RestaurantsInfo } from '../components/restaurant-info-card.component'
 import styled from 'styled-components/native';
 import { RestaurantsContext } from '../../../services/restaurants/restaurants.context';
+import { ActivityIndicator, Colors } from "react-native-paper";
 
 const SafeAreaContainer = styled.SafeAreaView`
  flex: 1;
@@ -18,12 +19,24 @@ const ListItemContainer = styled.View`
 const SearchContainer = styled(View)`
 padding: ${(props) => props.theme.space[3]};
 `;
-
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
 
 export const RestaurantsScreen = () => {
     const { restaurants, isLoading, error } = useContext(RestaurantsContext);
     return (
         <SafeAreaContainer>
+            {isLoading && (
+                <LoadingContainer>
+                    <Loading size={50} animating={true} color={Colors.blue300} />
+                </LoadingContainer>
+            )}
             <SearchContainer>
                 <Searchbar placeholder="Search" />
             </SearchContainer>
